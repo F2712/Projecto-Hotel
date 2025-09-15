748595c8d900310a9d8616646c701dc46796b708
Fran B <franciscobramati@gmail.com>
2025-09-15 17:58:13 -0300

    Fase muy temprana de la interfaz de información

--- src/componentes/Info.jsx
+++ src/componentes/Info.jsx
@@ -1,7 +1,16 @@
+import imag from "../assets/img/placeholder1.png";
+
 export function Info(){
     return (
         <>
-            <h1>Información</h1>
+            <div className="info">
+                <img src={imag}/>
+                <div className="demas">
+                    <p>información</p>
+                    <button>comprar</button>
+                    <button>alquilar</button>
+                </div>
+            </div>
         </>
     )
 }
