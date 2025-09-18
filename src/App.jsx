fd1a6cbb12dc2ba5240d59b11bfb7307d1110017
Mateo <cristofolmateo@gmail.com>
2025-09-18 16:41:41 -0300

    App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { Info } from './componentes/Info'
+import './App.css'
 
 function App() {
   return (
     <>
-      <div>
+      <div className="fondo">
         <Info/>
+        <p>hola</p>
       </div>
     </>
   )
