3921875fe44057628bc2249ba781ff61c7e69ef4
Fran B <franciscobramati@gmail.com>
2025-09-18 16:44:00 -0300

    boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { Info } from './componentes/Info'
+import { Boton_cambia } from './componentes/boton_cambia'
 
 function App() {
   return (
     <>
-      <div>
+      <div className='hola'>
+        <Boton_cambia dice='>' suma={1} max={20}/>
         <Info/>
       </div>
     </>
