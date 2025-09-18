175613633e1c7d0fa1cf2823c9d673ee14230400
Mateo <cristofolmateo@gmail.com>
2025-09-18 16:56:49 -0300

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ import './App.css'
 function App() {
   return (
     <>
-<<<<<<< HEAD
+
       <div className="fondo">
         <Info/>
         <p>chau</p>
-=======
+
       <div className='hola'>
         <Boton_cambia dice='>' suma={1} max={20}/>
         <div className="fondo">
           <Info/>
           <p>hola</p>
         </div>
->>>>>>> 36a74691835132b9d5ea77c6297ee36398452b14
+        </div>
       </div>
     </>
   )
