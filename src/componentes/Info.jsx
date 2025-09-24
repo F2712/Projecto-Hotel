b1ac3b4f32af59abf88493138c8b4899e94b1750
Gael <gaelleguizamonr@gmail.com>
2025-09-24 19:34:09 -0300

    a

--- src/componentes/Info.jsx
+++ src/componentes/Info.jsx
@@ -1,5 +1,6 @@
 // Json con la información de las habitaciones
 import habs from "../data/habitaciones.json";
+import "./info.css"
 
 // Numero de la habitación que se esté viendo en el momento
 let actual = 2;
@@ -17,10 +18,10 @@ export function Info(){
             <div className="info">
                 <img id='foto' src={AssetUrl(habs[actual].Foto)} alt="foto" />
                 <div className="demas">
-                    <h2>{habs[actual].Nombre}</h2>
+                    <h2 className="aah">{habs[actual].Nombre}</h2>
                     <p>{habs[actual].Info}</p>
-                    <button>comprar</button>
-                    <button>alquilar</button>
+                    <button className="botones">COMPRAR</button>
+                    <button className="botones">ALQUILAR</button>                  
                 </div>
             </div>
         </>
